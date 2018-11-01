af5a3470c138051ab3353ee3d40f59ce5d4ca044
EA <w0841821@apps.losrios.edu>
2018-11-01 00:56:34 -0700

    Basics

--- HW6/ttt.cpp
+++ HW6/ttt.cpp
@@ -17,6 +17,9 @@ using namespace std;
 // Congratulate the winner or declare a tie
 
 void startMenu();
+void instructions();
+bool coinFlip();
+void board();
 
 int main()
 {
@@ -24,46 +27,120 @@ int main()
   return 0;
 }
 
-// Specification C1 - Random Start
-// Determine which player will start the game - randomly. Make sure
-// you tell the client who’s starting.
-
-// Specification C2 - Simple Display
-// Display in a 2D table X, O and empty spaces.
-
-// Specification C3 - AI
-// Comment the start of your AI code which determines where the
-// computer will go during its turn.
-
-// Specification C4 - Decide Winner
-// Each move, check to see if somebody wins. If not, keep playing. If
-// so, declare winner and end game.
-
 // Specification B1 - Start Menu
-// Create a simple menu, 1 - Explain Game, 2 - Play, 3 - Quit. This menu will run once every time the program runs. Don’t go into excruciating detail explaining the game.
-
 void startMenu()
 {
-  char menuSelect;
+  int menuSelect;
+  do {
 
+  cout << "\n\n";
   cout << "1. How to play.\n";
   cout << "2. Play!\n";
   cout << "3. Quit.\n\n";
 
   cin >> menuSelect;
-}
 
 //Specification B2 - Valid Menu Choices
-// Filter the input for valid numbers. Re-prompt incorrect entries.
+  if (menuSelect == 1)
+  {
+    instructions();
+  }
+  else if (menuSelect == 2)
+  {
+    cout << "Let's play!!\n";
+    board();
+  }
+  else if (menuSelect == 3)
+  {
+    break;
+  }
+  else
+    cout << "\nOops, that wasn't an option. Try again.\n\n";
+
+// Specification A1 - Main Game Loop
+  } while (menuSelect != 3);
+
+}
+
+void instructions()
+{
+  cout << "\nInstructions!\n";
+  cout << "Player 1 (the user) will use X, while Player 2 (the computer) will use O.\n";
+  cout << "Get three symbols in a row, column, or diagonally, and you win!\n";
+}
+
+// Specification C1 - Random Start
+bool coinFlip()
+{
+  bool userFirst;
+  int whoGoesFirst = (rand() % 2);
+
+  if (whoGoesFirst == 0)
+  {
+    cout << "The user will go first!\n";
+    userFirst = true;
+  }
+  else
+  {
+    cout << "The computer will go first!\n";
+    userFirst = false;
+  }
+
+  return userFirst;
+}
+
+void board()
+{
+  char boardSymbol;
+  bool userFirst = coinFlip();
+  if (userFirst)
+    boardSymbol = 'X';
+  else
+    boardSymbol = 'O';
+  cout << "First symbol is " << boardSymbol << "\n";
+  //do {
+  char square[] = {'7', '8', '9', '4', '5', '6', '1', '2', '3'};
+
+/*
+  cout << "\n\n";
+  cout << "  " << square[0] << "  |  " << square[1] << "  |  " << square[2];
+  cout << "\n-----------------\n";
+  cout << "  " << square[3] << "  |  " << square[4] << "  |  " << square[5];
+  cout << "\n-----------------\n";
+  cout << "  " << square[6] << "  |  " << square[7] << "  |  " << square[8];
+  cout << "\n";
+  */
+
+  for (int i = 0; i < 9; i=i+3)
+  {
+    cout << "\n";
+    cout << "  " << square[i] << "  |  " << square[i+1] << "  |  " << square[i+2] << "\n";
+      if (i < 6)
+        cout << "-----------------";
+      //while (int j < 6)
+      //{
+        //cout << "\n-----------------\n";
+        //break;
+      //}
+  }
+
+  //} while (quitGame !)
+}
+
+// Specification C2 - Simple Display
+// Display in a 2D table X, O and empty spaces.
+
+// Specification C3 - AI
+// Comment the start of your AI code which determines where the computer will go during its turn.
+
+// Specification C4 - Decide Winner
+// Each move, check to see if somebody wins. If not, keep playing. If so, declare winner and end game.
 
 // Specification B3 - Pretty Display
 // Create a nicer display, with lines for the various boxes. Show the number inside the grid of valid moves, and X, and O of spots already taken. Stack this specification with specification C2 in your code (ie have both in your code - easier to give you the right grade.).
 
 // Specification B4 – Valid Move
 // Only allow the various players to make valid moves. This is more advanced input validation.
-
-// Specification A1 - Main Game Loop
-// Use a DO - WHILE to run the game until the client decides to quit.
 
 // Specification A2 - Simple Prompt
 // Just prompt at the end of the game if the human wants to play again (y/n).
