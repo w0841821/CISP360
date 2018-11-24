2009ed5ae0b169922bdeae21a264476e813f18a8
EA <w0841821@apps.losrios.edu>
2018-11-23 21:22:48 -0800

    Classes

--- HW7/madlibs.cpp
+++ HW7/madlibs.cpp
@@ -1,22 +1,5 @@
-//
-
-// Specification C1 - Random Word
-// Randomly select one of these words for VERB2: comb, paint, remove.
-
-// Specification C2 - Random Float
-// Generate a random float 1.0 to 15.0 inclusive. Display only 1 number after the decimal point.
-
-// Specification C3 - Word Menu
-// Display a simple menu for the client and allow them to only select one of the following words: breakfast, fishes, copper coins.
-
 // Specification B1 - Phrase Validation
 // FAMOUS PERSON and 2 WORD OCCUPATION require a word phrase. This assignment assumes this is just two words separated with a space. The input validation for this is to just check to see if the string has a space in it.
-
-//Specification B2 - Maximum Word Size Validation
-// NOUN1 can only be a maximum of 15 letters long.
-
-// Specification B3 - Range Validation
-// VERB5 must be between 4 and 20 characters in length.
 
 // Specification B4 – Simple Menu
 // Create a simple menu 1 - Explain Mad Libs, 2 - Start Mad Libs, 3 -Quit. Only allow valid choices.
@@ -24,122 +7,227 @@
 // Specification A1 - Main Game Loop
 // Use a DO - WHILE to run the game until the client decides to quit. Just prompt at the end of the game if the human want’s to play again (y/n).
 
-// Specification A2 - C Style String
-// Do not use a C++ style string for NOUN8, use a C Style string instead.
-
 // Specification A3 - Smallest Word
 // Prompt the client for 2 words. Select the smallest and use that for VERB3.
 
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
-void getWords();
-void printMadlib();
+class Madlib
+{
+	private:
+		string timespan,
+			adjective[2],
+			noun[7],
+			verb[6],
+			occupation,
+			person;
+		int integer;
+		float floatingNum;
+		char noun7[13],
+			letter;
+	public:
+		void getWords();
+		void printStory();
+};
+
+string noun0();
+string noun1();
+string verb1();
+string verb5();
+float roomNum();
 
 int main()
 {
-  getWords();
-  printMadlib();
-  return 0;
+	srand(time(0));
+	Madlib story;
+
+	story.getWords();
+	story.printStory();
+
+	return 0;
 }
 
-void getWords()
+void Madlib::getWords()
 {
-	string timespan,
-		adjective[2],
-		noun[8],
-		verb[7],
-		occupation,
-		person;
-	int integer;
-	float floatNum;
-	char letter;
+	cout << "Give me a timespan. ";
+	cin >> timespan;
 
-  cout << "Give me a time-span.\n";
-  cin >> timespan;
+	cout << "Give me a noun, no longer than 15 letters. ";
+	noun[0] = noun0();
 
-	cout << "Give me a noun.\n";
-  cin >> noun[0];
+	cout << "Give me a verb. ";
+	cin >> verb[0];
 
-	cout << "Give me a verb.\n";
-  cin >> verb[0];
+//	cout << "Give me a verb. ";
+	verb[1] = verb1();
 
-  cout << "Give me a verb.\n";
-  cin >> verb[1];
+//	cout << "Give me a noun (plural or singular). ";
+//	cin >> noun[1];
+	noun[1] = noun1();
 
-  cout << "Give me a noun (plural or singular).\n";
-  cin >> noun[1];
+	cout << "Give me a noun. ";
+	cin >> noun[2];
 
-  cout << "Give me a noun.\n";
-  cin >> noun[2];
+	cout << "Give me a verb ending in \"ing\" (eg: running, walking). ";
+	cin >> verb[2];
 
-	cout << "Give me a verb ending in \"ing\" (eg: running, walking).\n";
-  cin >> verb[2];
+	cout << "Give me a two-word occupation. ";
+	cin.ignore();
+	getline(cin, occupation);
 
-	cout << "Give me a two-word occupation.\n";
-  cin.ignore();
-  getline(cin, occupation);
+	cout << "Give me a noun. ";
+	cin >> noun[3];
 
-  cout << "Give me a noun.\n";
-  cin >> noun[3];
+	cout << "Give me a noun. ";
+	cin >> noun[4];
 
-  cout << "Give me a noun.\n";
-  cin >> noun[4];
+	cout << "Give me a famous person (first and last name). ";
+	cin.ignore();
+	getline(cin, person);
 
-	cout << "Give me a famous person (first and last name).\n";
-  cin.ignore();
-  getline(cin, person);
+	cout << "Give me an adjective. ";
+	cin >> adjective[0];
 
-  cout << "Give me an adjective.\n";
-  cin >> adjective[0];
+	cout << "Give me a verb. ";
+	cin >> verb[3];
 
-  cout << "Give me a verb.\n";
-  cin >> verb[3];
+	cout << "Give me a noun. ";
+	cin >> noun[5];
 
-  cout << "Give me a noun.\n";
-  cin >> noun[5];
+	cout << "Give me a noun (plural). ";
+	cin >> noun[6];
 
-  cout << "Give me a plural noun.\n";
-  cin >> noun[6];
+	cout << "Give me an adjective. ";
+	cin >> adjective[1];
 
-  cout << "Give me an adjective.\n";
-  cin >> adjective[1];
+	cout << "Give me a noun (plural). ";
+	// Specification A2 - C Style String
+	cin.ignore();
+	cin.getline(noun7, 13);
+	//cin >> setw(13) >> noun7;
 
-  cout << "Give me a plural noun.\n";
-  cin >> noun[7];
+	cout << "Give me a verb with preposition (eg: wait for, hang with). ";
+//	cin.ignore();
+	getline(cin, verb[4]);
 
-  cout << "Give me a verb with preposition (eg: waiting for, going with, smells of, based on).\n";
-  cin.ignore();
-  getline(cin, verb[5]);
+	cout << "Give me an integer. (eg: 1, 20, 100) ";
+	cin >> integer;
 
-	cout << "Give me an integer.\n";
-  cin >> integer;
+	cout << "Give me a verb. ";
+//	cin >> verb[5];
+	verb[5] = verb5();
 
-	cout << "Give me a verb.\n";
-  cin >> verb[6];
+//	cout << "Give me a floating number (eg: 4.1). ";
+//	cin >> floatingNum;
+	floatingNum = roomNum();
 
-	cout << "Give me a floating number (eg: 4.1).\n";
-  cin >> floatNum;
-
-	cout << "Give me a single letter.\n";
+	cout << "Give me a single letter. ";
 	cin >> letter;
+	letter = toupper(letter);
 }
 
-void printMadlib()
+//Specification B2 - Maximum Word Size Validation
+string noun0()
 {
-  cout << "  A Day In The Life Of a College Student\n";
-  cout << "  ======================================\n\n";
-  cout << "Every " << timespan << ", I wake up and get off my " << noun[0] << ". I don't\n";
-  cout << "always have time to " << verb[0] << " a shower, but I always make sure\n";
-  cout << "to " << verb[1] << " my hair. Once that‘s done I get some " << noun[1] ",\n";
-  cout << "and then it‘s off to my first " << noun[2] << ". I often\n";
-  cout << "get caught " << verb[2] << " in class. No one likes it\n";
-  cout << "when the " << occupation << " gives a surprise " << noun[3] << ". My second\n";
-  cout << "class is " << noun[4] << " Theory. Dr. " << person << " is incredibly\n";
-  cout << adjective[0] << ", but I never " << verb[3] << " her because of her thick " << noun[5] << ".\n";
-  cout << "Her " << noun[6] << ", however, are really " << adjective[1] << ". After\n";
-  cout << "lunch, I have no more " << noun[7] << " and I‘m free to " << verb[4] << "\n";
-  cout << "my friends. This is right around " << integer << " O’clock.\n";
-  cout << "Before I go to bed I " << verb[5] << " a little bit, in my room, " << floatNum << " " << letter << ".\n";
+	string noun;
+	int size;
+
+	do {
+		cin >> noun;
+		size = noun.length();
+		if (size > 15)
+			cout << "\nThat word is too long. Try again.\n";
+
+	// loop while the word is longer than 15 characters
+	} while(size > 15);
+
+	return noun;
+}
+
+// Specification C3 - Word Menu
+string noun1()
+{
+	char menu;
+	string noun;
+
+	do {
+		cout << "Pick from the following: (b)reakfast, (f)ishes, (c)opper coins. ";
+		cin >> menu;
+		switch(menu)
+		{
+			case 'b':
+			case 'B': noun = "breakfast";
+			break;
+			case 'f':
+			case 'F': noun = "fishes";
+			break;
+			case 'c':
+			case 'C': noun = "copper coins";
+			break;
+			default: cout << "\nNot a valid entry. Try again.\n";
+		}
+	} while((toupper(menu) != 'B') && (toupper(menu) != 'F') && (toupper(menu) != 'C'));
+
+	return noun;
+}
+
+// Specification C1 - Random Word
+string verb1()
+{
+	string str;
+	int theFlip = (rand() % 3);
+	if (theFlip == 0)
+		str = "comb";
+	else if (theFlip == 1)
+		str = "paint";
+	else
+		str = "remove";
+	return str;
+}
+
+// Specification B3 - Range Validation
+string verb5()
+{
+	string verb;
+	int size;
+
+	do {
+		cin >> verb;
+		size = verb.length();
+		if (size < 4 || size > 20)
+			cout << "\nThe word must be between 4 and 20 characters. Try again.\n";
+
+	// loop while the word is not between 4 and 20 characters
+	} while(size < 4 || size > 20);
+
+	return verb;
+}
+
+// Specification C2 - Random Float
+float roomNum()
+{
+	float i = (rand() % 141) + 10;
+	i = i / 10;
+	return i;
+}
+
+void Madlib::printStory()
+{
+	cout << "  A Day In The Life Of a College Student\n";
+    cout << "  ======================================\n\n";
+    cout << "Every " << timespan << ", I wake up and get off my " << noun[0] << ". I don't\n";
+    cout << "always have time to " << verb[0] << " a shower, but I always make sure\n";
+    cout << "to " << verb[1] << " my hair. Once that‘s done I get some " << noun[1] << ",\n";
+    cout << "and then it‘s off to my first " << noun[2] << ". I often\n";
+    cout << "get caught " << verb[2] << " in class. No one likes it\n";
+    cout << "when the " << occupation << " gives a surprise " << noun[3] << ". My second\n";
+    cout << "class is " << noun[4] << " Theory. Dr. " << person << " is incredibly\n";
+    cout << adjective[0] << ", but I never " << verb[3] << " her because of her thick " << noun[5] << ".\n";
+    cout << "Her " << noun[6] << ", however, are really " << adjective[1] << ". After\n";
+    cout << "lunch, I have no more " << noun7 << " and I‘m free to " << verb[4] << "\n";
+    cout << "my friends. This is right around " << integer << " O’clock.\n";
+    cout << "Before I go to bed I " << verb[5] << " a little bit, in my room, " << floatingNum << " " << letter << ".\n";
 }
