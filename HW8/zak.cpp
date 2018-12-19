cf80c29ea76fb2a74326a9625b6a01adb23432f1
EA <w0841821@apps.losrios.edu>
2018-12-18 22:04:50 -0800

    Update?

--- HW8/zak.cpp
+++ HW8/zak.cpp
@@ -2,60 +2,88 @@
 #include <string>
 using namespace std;
 
-int numStudents();
-int numQuizzes();
-void getStudentNames(int &);
+//void getZak(string**);
+
+class Zak
+{
+	private:
+//		const int COLS = students;
+//		const int ROWS = quizzes;
+
+		int students,
+			quizzes;
+
+	public:
+//		void start();
+		string grades[students][quizzes];
+		void sayHi();
+		void numStudents();
+		void numQuizzes();
+		void printGrades();
+};
 
 int main()
 {
-	int students = numStudents();
-	int student[students];
-	
-	int quizzes = numQuizzes();
-	int quiz[quizzes];
-	
-	cout << "Students: " << students << "\n";
-	cout << "Quizzes: " << quizzes << "\n";
-	
-	getStudentNames(students);
-	
+	Zak grades;
+	grades.sayHi();
+	grades.numStudents();
+	grades.numQuizzes();
+	grades.printGrades();
+	//string grades[students][quizzes];
 	return 0;
 }
 
-int numStudents()
+void Zak::sayHi()
 {
-	int students;
-	
+	cout << "\nWelcome to your grading program, Professor Zak!\n\n";
+}
+
+void Zak::numStudents()
+{
 	cout << "\nStudents? ";
 	cin >> students;
-	
-	return students;
 }
 
-int numQuizzes()
+void Zak::numQuizzes()
 {
-	int quizzes;
-	
 	cout << "\nQuizzes? ";
 	cin >> quizzes;
-	
-	return quizzes;
 }
 
-void getStudentNames(int &students)
+void Zak::printGrades()
 {
-	string studentName[students];
-	
-	for (int i = 0; i < students; i++)
+	int i,
+		j;
+
+	for (i = 0; i < students; i++)
+	{
+		j = 0;
+		cout << "Stud: " << i;
+		for (j = 0; j < quizzes; j++)
+		{
+			cout << "Quiz: " << j;
+		}
+	}
+}
+/*
+void getZak(string grades[][quizzes])
+{
+	int i,
+		j = 0;
+
+	cout << "We have " << students << " students, and " << quizzes << " quizzes.\n";
+
+	for (i = 0; i < students; i++)
 	{
 		cout << "What is student #" << i + 1 << "'s name? ";
-		cin >> studentName[i];
-	}
-	
-	for (int i = 0; i < students; i++)
-	{
-		cout << "Student #" << i + 1 << ": " << studentName[i] << "\n";
+		cin >> grades[i][j];
+
+		cout << "What was their grade for quiz #...\n";
+		for (j = 0; j < quizzes; j++)
+		{
+			cout << j + 1;
+			cin >> grades[i][j];
+		}
 	}
 }
-
-		
+*/
